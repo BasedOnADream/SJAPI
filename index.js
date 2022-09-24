7ce049ca78ef716822cd792a7f5e84e8ebbe4617
Aleksy Kosiński <kosinski.aleks@gmail.com>
2022-09-24 19:45:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,145 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT =  7005;
+
+const getData = () => fs.readFileSync("Data.json", "utf8");
+let DataTemp = getData();
+
+const Append = (p) => 
+{
+    DataTemp = getData()
+    fs.writeFileSync("Data.json","[")
+    for(let i of JSON.parse(DataTemp))
+    {
+        fs.appendFileSync("Data.json", (JSON.stringify(i)+","))
+    }
+    fs.appendFileSync("Data.json", JSON.stringify(p)+"]")
+    DataTemp = getData();
+}
+
+const Check = (uid) =>
+{
+    for(let i of JSON.parse(getData()))
+    {
+        if(i.id === parseInt(uid))
+        {
+            return i;
+        }
+    }
+}
+
+app.get("/getAllUsers", (req, res) =>
+{
+    res.status(200).send(JSON.parse(getData()))
+})
+
+app.use(express.json())
+app.post("/addUser", (req,res) =>
+{
+    const {id, name, surname, age, nationality} = req.body;
+
+    if(!name || !id || !surname || !age || !nationality)
+    {
+        res.status(400).send({request:"Failed",message:"You have some parameters missing! Check your JSON."})
+    }
+    else
+    {
+        res.status(200).send(
+        {
+            request:"Accepted",
+            message:`User ${name+" "+surname} was added!`
+        })
+        Append(req.body)
+    }
+})
+
+
+
+app.get("/getUser/:uid", (req, res) =>
+{
+    const {uid} = req.params;
+
+    let Response = Check(uid);
+
+    if(Response)
+    {
+        res.status(200).send(Response);
+    }
+    else
+    {
+        res.status(400).send({request:"Failed",message:"The requested user dosen't exists!"})
+    }
+})
+
+app.delete("/popUser", (req,res) =>
+{
+    if(JSON.parse(getData()).length > 0)
+    {
+        let popedArray = JSON.parse(getData());
+        popedArray.pop()
+        fs.writeFileSync("Data.json", JSON.stringify(popedArray));
+        res.status(200).send({request:"Success",message:"User was poped!"})
+    }
+    else
+    {
+        res.status(400).send({request:"Failed",message:"There are no more users to pop!"})
+    }
+})
+
+app.delete("/removeUser/:uid", (req,res) =>
+{
+    const {uid} = req.params;
+
+    if(JSON.parse(getData()).length > 0)
+    {
+        DataTemp = [];
+        JSON.parse(getData()).map(i=>
+        {
+            i.id == parseInt(uid) ? 0 : DataTemp.push(i)
+        },0)
+        fs.writeFileSync("Data.json", JSON.stringify(DataTemp));
+        res.status(200).send({request:"Success",message:"User was removed!"})
+    }
+    else
+    {
+        res.status(400).send({request:"Failed",message:"There are no more users to delete!"})
+    }
+})
+
+app.patch("/updateUser", (req, res) =>
+{
+    const {id, name, surname, age, nationality} = req.body;
+
+    const exists = Check(id)
+    if(exists)
+    {
+        if(!name || !id || !surname || !age || !nationality)
+        {
+            res.status(400).send({request:"Failed",message:"You have some parameters missing!"})
+        }
+        else
+        {
+            DataTemp = [];
+            for(let i of JSON.parse(getData()))
+            {
+                if(i.id === id)
+                {
+                    DataTemp.push({id,name,surname,age,nationality})
+                }
+                else
+                {
+                    DataTemp.push(i)
+                }
+            }
+            fs.writeFileSync("Data.json", JSON.stringify(DataTemp));
+            res.status(200).send({request:"Success",message:"User has been updated!"})
+        }
+    }
+    else
+    {
+        res.status(400).send({request:"Failed",message:"This user dosen't exists!"})
+    }
+})
+
+app.listen(PORT, () =>console.log("API is ready!"));
---
+++ readme.md
@@ -0,0 +1,39 @@
+# SJAPI(Simple JSON API)
+
+---
+
+This API can:
+
+- Read users
+- Update users
+- Delete users
+- Pop users(remove last user)
+- Add users
+
+---
+
+API dosen't have any authorisation or protection.
+All user data is stored inside Data.json.
+To start a server open terminal with API directory in it and type `npm start` or by Using ServerStart.bat.
+The server will open on port 7005 and you can access it in your browser by going to [here](http://localhost:7005).
+To close the server use ServerKill.bat or type `npm stop` in CMD.
+
+---
+
+You can access the non-body requests here:
+
+- [Get all users](http://localhost:7005/getAllUsers)
+- [Get user by ID default 1](http://localhost:7005/getUser/1)
+- [Pop an user](http://localhost:7005/popUser)
+- [Remove an user by ID default 1](http://localhost:7005/removeUser/1)
+
+---
+
+These request require a body, I recommend to use [Insomnia](https://insomnia.rest/) for these or pure code: 
+
+- Add a user: http://localhost:7005/addUser
+- Update a user: http://localhost:7005/updateUser
+
+---
+
+API was made using *Node.js*,*Express.js*,*File System*
